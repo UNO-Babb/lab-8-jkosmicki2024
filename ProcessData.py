9f5c8b73d9a6863b7db49b67afa4381bca361b5b
jkosmicki2024 <jkosmicki@unomaha.edu>
2025-04-02 14:24:47 +0000

    Done with lab

--- ProcessData.py
+++ ProcessData.py
@@ -1,23 +1,49 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name: Jacob Kosmicki
+#Date: 4/2/2025
+#Assignment: Lab 08
 
 import random
 
 def main():
 
-  #Open the files we will be using
+  
   inFile = open("names.dat", 'r')
   outFile = open("StudentList.csv", 'w')
+  for line in inFile:
+    data = line.split()
+    first = data[0]
+    last = data[1]
+    IDNUM = data[3]
+    year = data[5]
+    if (year.find("F")!=-1):
+      year="FR"
+    elif (year.find("h")!=-1):
+      year="SO"
+    elif (year.find("J")!=-1):
+      year="JR"
+    elif (year.find("i")!=-1):
+      year="SR"
 
-  #Process each line of the input file and output to the CSV file
-
-
-
-  #Close files in the end to save and ensure they are not damaged.
+    major = data[6]
+    abrv_Major = major[0:3]
+    
+    student_id = makeID(first, last, IDNUM, abrv_Major,"-",year)
+    output = data[0]
+    outFile.write(output)
+    
+    print(student_id)
   inFile.close()
   outFile.close()
+
+def makeID(first, last, IDNUM, major, dash, year):
+  print(first, last, IDNUM, major, dash, year)
+  idlen = len(IDNUM) 
+
+  while len(last) < 5:
+    last = last + "X"
+  id = first[0] + last + IDNUM[idlen - 3: ]
+  return id 
 
 if __name__ == '__main__':
   main()
